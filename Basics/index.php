f3c1da4200bf294bfb3de42bf10163bad1fcc304
nishantp15 <prajapati.nishant@outlook.com>
2023-05-21 13:31:16 +0530

    conditional op

--- Basics/index.php
+++ Basics/index.php
@@ -1,16 +1,48 @@
 <?php
-    $age = 16;
+// $age = 16;
 
-    if($age>18){
-        echo "You may enter the site";
-    }
-    elseif($age){
-        echo "You are not allowed to enter";
-    }
-    else{
-        echo "invalid age";
+// if($age>18){
+//     echo "You may enter the site";
+// }
+// elseif($age){
+//     echo "You are not allowed to enter";
+// }
+// else{
+//     echo "invalid age";
 
-    }
+// }
 
+// // logical operators
+//   $temp = 25;
+//   $cloudy = true;
 
-?>
+//   if($temp>=0 && $temp<=33){
+//     echo "Weather is good";
+//   }else{
+//     echo "weather is bad";
+//   }
+
+//   if(!$cloudy){
+//     echo "It's sunny";
+//   }
+
+// // switch
+
+$grade = "B";
+
+switch ($grade) {
+    case "A":
+        echo "You did Great!";
+        break;
+    case "B":
+        echo "You did Well!";
+        break;
+    case "c":
+        echo "You did Fine!";
+        break;
+    case "F":
+        echo "You did Failed!";
+        break;
+    default:
+    echo "invalid";
+}
